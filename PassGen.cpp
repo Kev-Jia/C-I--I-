cdcfb264a50e13b1327c7c7767d93a2a8a4c8bb7
Kevin Jia <kevinhengkaijia@hotmail.com>
2016-12-19 22:19:10 +0000

    Add files via upload

---
+++ PassGen.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+static const char alphanum[] =
+{
+    "0123456789"
+    "!&*()_+{}:@$%~<>?|"
+    "-=[];'#,."
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+};
+int size = sizeof(alphanum) - 1;
+
+int main()
+{
+    char password;
+    cout << endl;
+    cout << "Password: ";
+    int length = 8;
+	
+    srand(time(NULL));
+    for (int i = 0; i < length; i++)
+    {
+        password = alphanum[rand() % size];
+        cout << password;
+    }
+    cout << endl << endl;
+    return 0;
+}
